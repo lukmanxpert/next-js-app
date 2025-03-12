7f88efdb60520a96c8846728c2800d258f287f65
lukmanxpert <lukmanmiah2004@gmail.com>
2025-03-12 15:07:21 +0600

    navigate and condition navigating

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const NavBar = () => {
     return (
         <div className=''>
             <ul className='flex justify-evenly items-center text-3xl'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Contact</li>
+                <Link href={"/"}>
+                    <li>Home</li>
+                </Link>
+                <Link href={"/services"}>
+                    <li>Services</li>
+                </Link>
+                <Link href={"/contact"}>
+                    <li>Contact</li>
+                </Link>
             </ul>
         </div>
     );
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const ContactPage = () => {
+    const router = useRouter();
+    const user = false;
+    const handleAddressNavigate = () => {
+        if (user) {
+            router.push("/contact/address")
+        } else {
+            router.push("/")
+        }
+    }
     return (
         <div>
-            This is contact ServicePages!
+            <h1>This is contact ServicePages!</h1>
+            <button className='text-2xl font-bold border-2 rounded-2xl m-2 p-2' onClick={handleAddressNavigate} type='button'>Address</button>
         </div>
     );
 };
